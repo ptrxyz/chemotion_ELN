eaed12d15907260340418002772e21b9ba30d547
Thomas Watolla <tw@megorei.com>
2022-01-13 17:52:59 +0100

    Fix Wellplate readouts init

--- app/packs/src/components/models/Wellplate.js
+++ app/packs/src/components/models/Wellplate.js
@@ -111,7 +111,7 @@ export default class Wellplate extends Element {
     return {
       ...well,
       position: this.calculatePositionOfWellByIndex(i),
-      readouts: []
+      readouts: well.readouts || []
     };
   }
 
