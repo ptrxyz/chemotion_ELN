a202cc5d332524355a77ec6afacbdc018bfd24cc
Johannes Haubold <jh@megorei.com>
2022-04-01 17:02:57 +0200

    Improve Measurement API Service to allow return of data the frontend can use to display the results

--- app/usecases/measurements/bulk_create_from_raw_data.rb
+++ app/usecases/measurements/bulk_create_from_raw_data.rb
@@ -4,12 +4,19 @@ module Usecases
   module Measurements
     class BulkCreateFromRawData
       attr_reader :raw_data, :current_user
+      ERRORS = {
+        permission_error: 'Permission Error - You are not allowed to update this sample',
+        sample_not_found: 'Could not find sample',
+        missing_fields: 'Data Error - description, sample identifier, unit and value data required',
+        sample_identifier_ambiguous: 'Sample identifier matches more than one sample, measurement cannot be assigned correctly',
+      }
 
       # raw_data: 
       # [
       #   {
+      #     uuid # an artificial identifier only required for the frontend, can be ignored
       #     description
-      #     sample_id
+      #     sample_identifier
       #     unit
       #     value
       #   }
@@ -20,33 +27,66 @@ module Usecases
       end
 
       def execute!
-        permission_error unless ElementsPolicy.new(current_user, samples).update?
-        data_error unless raw_data.all? { |entry| required_fields_present?(entry) }
+        raw_data.map do |entry|
+          entry['errors'] ||= []
+          check_sample_availability!(entry)
+          check_sample_permissions!(entry)
+          create_measurement!(entry)
 
-        Measurement.create!(raw_data)
+          entry
+        end
       end
 
       private
 
-      def samples
-        @samples ||= begin
-          sample_ids = raw_data.map { |entry| entry['sample_id'] }.compact.uniq
-          sample_ids.any? ? Sample.where(id: sample_ids) : Sample.none
+      def check_sample_permissions!(entry)
+        return if entry_has_errors?(entry)
+
+        sample = Sample.find_by(short_label: entry['sample_identifier'])
+
+        entry['errors'] << ERRORS[:permission_error] unless !ElementsPolicy.new(current_user, sample).update?
+      end
+
+      def create_measurement!(entry)
+        return if entry_has_errors?(entry)
+
+        sample = Sample.find_by(short_label: entry['sample_identifier'])
+        measurement = Measurement.build(
+          sample: sample,
+          description: entry['description'],
+          unit: entry['unit'],
+          value: entry['value']
+        )
+        if (measurement.valid?)
+          measurement.save!
+          entry['id'] = measurement.id
+        else
+          measurement.errors.full_messages.each { |message| entry['errors'] << message }
+        end
+
+        entry
+      end
+
+      def required_fields_missing?(entry)
+        %w[description sample_identifier unit value].all? do |key|
+          entry.key?(key) && entry[key].present?
         end
       end
 
-      def permission_error
-        # TODO
-        raise 'Permission Error - One or more samples can not be updated'
+      def check_required_fields!(entry)
+        entry['errors'] << ERRORS[:missing_fields] if required_fields_missing?(entry)
       end
 
-      def required_fields_present?(entry)
-        (entry.keys.sort == %w[description sample_id unit value]) && entry.values.none?(&:blank?)
+      def check_sample_availability!(entry)
+        count = Sample.where(short_label: entry['sample_identifier']).count
+        return if count == 1
+
+        entry['errors'] << ERRORS[:sample_not_found] if count.zero?
+        entry['errors'] << ERRORS[:sample_identifier_ambiguous] if count > 1
       end
 
-      def data_error
-        # TODO
-        raise 'Data Error - all entries need description, sample_id, unit and value data'
+      def entry_has_errors?(entry)
+        return entry['errors'].size > 0
       end
     end
   end
