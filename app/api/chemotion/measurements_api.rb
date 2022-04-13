f7f26c87cc826feb9467332a53cb356fc5e429ec
Johannes Haubold <jh@megorei.com>
2022-04-13 10:08:43 +0200

    Remove currently unused api endpoints

--- app/api/chemotion/measurements_api.rb
+++ app/api/chemotion/measurements_api.rb
@@ -74,42 +74,6 @@ module Chemotion
         results
       end
 
-      namespace :bulk_create_from_well do
-        params do
-          requires :well_id, type: Integer, desc: 'Well ID'
-        end
-        post do
-          well = Well.find(params[:well_id])
-          sample = well.sample
-          error!('401 Unauthorized', 401) unless ElementPolicy.new(current_user, well.wellplate).read?
-          error!('401 Unauthorized', 401) unless ElementPolicy.new(current_user, sample).update?
-
-          Usecases::Measurements::BulkCreateFromWell.new(well).execute!
-
-          {}
-        rescue StandardError => e
-          error!(e, 500)
-        end
-      end
-
-      namespace :bulk_create_from_wellplate do
-        params do
-          requires :wellplate_id, type: Integer, desc: 'Wellplate ID'
-        end
-        post do
-          wellplate = Wellplate.includes(wells: [:sample]).find(params[:wellplate_id])
-          samples = Sample.where(id: wellplate.wells.map(&:sample_id))
-          error!('401 Unauthorized', 401) unless ElementPolicy.new(current_user, wellplate).read?
-          error!('401 Unauthorized', 401) unless ElementsPolicy.new(current_user, samples).update?
-
-          Usecases::Measurements::BulkCreateFromWellplate.new(wellplate).execute!
-
-          {}
-        rescue StandardError => e
-          error!(e, 500)
-        end
-      end
-
       namespace :bulk_create_from_raw_data do
         params do
           requires :raw_data, type: Array do
