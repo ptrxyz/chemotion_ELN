b713195e1cb9deae854e8656b67fe7070f69cb1b
Mehreen <mehreen.mansur>
2022-08-10 11:57:14 +0200

    add rspec for add and update tabs segment

--- app/api/chemotion/collection_api.rb
+++ app/api/chemotion/collection_api.rb
@@ -479,7 +479,7 @@ module Chemotion
         desc 'Update tab segment'
         params do
           requires :id, type: Integer, desc: 'Collection id'
-          optional :segment, type: String, desc: 'Tab segment type'
+          optional :segment, type: Hash, desc: 'Tab segment type'
         end
 
         patch do
